8fca6ff07308b5800f34bbdc746442e5e21bb8cf
Aaron Son <aaron@dolthub.com>
2021-10-01 14:32:31 -0700

    go/store/datas: commit_skip_list.go: Wrap some commit skip list logic a bit.

--- go/store/datas/commit_skip_list.go
+++ go/store/datas/commit_skip_list.go
@@ -24,7 +24,77 @@ import (
 	"github.com/dolthub/dolt/go/store/types"
 )
 
-func getParentsSkipListHeight(ctx context.Context, parents types.List) (int, error) {
+type ParentsSkipList struct {
+	types.Tuple
+}
+
+func (p ParentsSkipList) Len() uint64 {
+	return p.Tuple.Len()
+}
+
+func (p ParentsSkipList) Get(n uint64) (SkipListNode, error) {
+	v, err := p.Tuple.Get(n)
+	if err != nil {
+		return SkipListNode{}, err
+	}
+	t, ok := v.(types.Tuple)
+	if !ok {
+		return SkipListNode{}, errors.New("unexpected element in skip list entry Tuple; not a Tuple")
+	}
+	return SkipListNode{t}, nil
+}
+
+type SkipListNode struct {
+	types.Tuple
+}
+
+func (n SkipListNode) Height() uint64 {
+	return n.Tuple.Len()
+}
+
+func (n SkipListNode) GetRef(i uint64) (types.Ref, bool, error) {
+	v, err := n.Get(i)
+	if err != nil {
+		return types.Ref{}, false, err
+	}
+	if types.IsNull(v) {
+		return types.Ref{}, false, nil
+	}
+	r, ok := v.(types.Ref)
+	if !ok {
+		return types.Ref{}, false, errors.New("unexpected element in skip list entry Tuple; not Null or Ref")
+	}
+	return r, true, nil
+}
+
+func LoadParentsSkipListFromCommitRef(ctx context.Context, vrw types.ValueReadWriter, ref types.Ref) (ParentsSkipList, bool, error) {
+	val, err := ref.TargetValue(ctx, vrw)
+	if err != nil {
+		return ParentsSkipList{}, false, err
+	}
+	commit, ok := val.(types.Struct)
+	if !ok {
+		return ParentsSkipList{}, false, errors.New("expected ref to be a Commit Struct; was not")
+	}
+	return LoadParentsSkipListFromCommit(commit)
+}
+
+func LoadParentsSkipListFromCommit(commit types.Struct) (ParentsSkipList, bool, error) {
+	val, found, err := commit.MaybeGet(ParentsSkipListField)
+	if err != nil {
+		return ParentsSkipList{}, false, err
+	}
+	if !found {
+		return ParentsSkipList{}, false, nil
+	}
+	tuple, ok := val.(types.Tuple)
+	if !ok {
+		return ParentsSkipList{}, false, errors.New("unexpected parents skip list entry; not a Tuple")
+	}
+	return ParentsSkipList{tuple}, true, nil
+}
+
+func SkipListNodeHeightForParents(ctx context.Context, parents types.List) (int, error) {
 	h := sha512.New()
 	err := parents.IterAll(ctx, func(v types.Value, i uint64) error {
 		r, ok := v.(types.Ref)
@@ -44,39 +114,22 @@ func getParentsSkipListHeight(ctx context.Context, parents types.List) (int, err
 	if ret == 32 {
 		ret -= 1
 	}
-	return ret, nil
+	return ret + 1, nil
 }
 
-func getSkipListNodeFromCommitRef(ctx context.Context, vrw types.ValueReadWriter, r types.Ref) (types.Tuple, error) {
-	tv, err := r.TargetValue(ctx, vrw)
+func getSkipListNodeFromCommitRef(ctx context.Context, vrw types.ValueReadWriter, r types.Ref) (SkipListNode, error) {
+	sl, found, err := LoadParentsSkipListFromCommitRef(ctx, vrw, r)
 	if err != nil {
-		return types.EmptyTuple(vrw.Format()), err
+		return SkipListNode{}, err
 	}
-	commit := tv.(types.Struct)
-	slnVal, found, err := commit.MaybeGet(ParentsSkipListField)
-	if !found {
-		return types.EmptyTuple(vrw.Format()), nil
+	if !found || sl.Len() == 0 {
+		return SkipListNode{types.EmptyTuple(vrw.Format())}, nil
 	}
-	slnTuple, ok := slnVal.(types.Tuple)
-	if !ok {
-		return types.EmptyTuple(vrw.Format()), errors.New("unexpected parents skip list entry; not a Tuple")
-	}
-	if slnTuple.Len() == 0 {
-		return types.EmptyTuple(vrw.Format()), nil
-	}
-	cVal, err := slnTuple.Get(0)
-	if err != nil {
-		return types.EmptyTuple(vrw.Format()), err
-	}
-	ret, ok := cVal.(types.Tuple)
-	if !ok {
-		return types.EmptyTuple(vrw.Format()), errors.New("unexpected first element in skip list entry Tuple; not a Tuple")
-	}
-	return ret, nil
+	return sl.Get(0)
 }
 
 func getParentSkipListNodeOfHeight(ctx context.Context, vrw types.ValueReadWriter, parentRef types.Ref, height int) (types.Tuple, error) {
-	// Return a tuple of length (height + 1). The i-th entry in the Tuple
+	// Return a tuple of length height. The i-th entry in the Tuple
 	// will point to a RefOfValue for the first commit found, starting at
 	// parentRef and walking it's first parents_skip_list entries, where
 	// the ref'd Commit's parents_skip_list entry itself has at least level
@@ -87,14 +140,14 @@ func getParentSkipListNodeOfHeight(ctx context.Context, vrw types.ValueReadWrite
 	var err error
 	currRef, err := types.ToRefOfValue(parentRef, vrw.Format())
 	if err != nil {
-		return types.EmptyTuple(vrw.Format()), err
+		return types.Tuple{}, err
 	}
 
-	if height == 0 {
+	if height == 1 {
 		return types.NewTuple(vrw.Format(), currRef)
 	}
 
-	entries := make([]types.Value, height+1)
+	entries := make([]types.Value, height)
 	for i := range entries {
 		if i == 0 {
 			entries[i] = currRef
@@ -103,29 +156,27 @@ func getParentSkipListNodeOfHeight(ctx context.Context, vrw types.ValueReadWrite
 		}
 	}
 
-	currSkipListNode, err := getSkipListNodeFromCommitRef(ctx, vrw, currRef)
+	currNode, err := getSkipListNodeFromCommitRef(ctx, vrw, currRef)
+	if err != nil {
+		return types.Tuple{}, err
+	}
 ENTRIES:
-	for i := 1; i <= height; i++ {
-		for i+1 > int(currSkipListNode.Len()) {
-			if currSkipListNode.Len() == 0 {
+	for i := 1; i < height; i++ {
+		for int(currNode.Height()) < i + 1 {
+			if currNode.Height() == 0 {
 				break ENTRIES
 			}
-			crVal, err := currSkipListNode.Get(currSkipListNode.Len() - 1)
+			var found bool
+			currRef, found, err = currNode.GetRef(currNode.Height() - 1)
 			if err != nil {
-				return types.EmptyTuple(vrw.Format()), err
+				return types.Tuple{}, err
 			}
-			if types.IsNull(crVal) {
+			if !found {
 				break ENTRIES
 			}
-			var ok bool
-			currRef, ok = crVal.(types.Ref)
-			if !ok {
-				return types.EmptyTuple(vrw.Format()), errors.New("unexpected last element in skip list node; not Null or Tuple")
-			}
-
-			currSkipListNode, err = getSkipListNodeFromCommitRef(ctx, vrw, currRef)
+			currNode, err = getSkipListNodeFromCommitRef(ctx, vrw, currRef)
 			if err != nil {
-				return types.EmptyTuple(vrw.Format()), err
+				return types.Tuple{}, err
 			}
 		}
 		entries[i] = currRef
@@ -139,9 +190,9 @@ func getParentsSkipList(ctx context.Context, vrw types.ValueReadWriter, parents 
 		return types.EmptyTuple(vrw.Format()), nil
 	}
 
-	parentHeight, err := getParentsSkipListHeight(ctx, parents)
+	parentHeight, err := SkipListNodeHeightForParents(ctx, parents)
 	if err != nil {
-		return types.EmptyTuple(vrw.Format()), nil
+		return types.Tuple{}, err
 	}
 
 	entries := make([]types.Value, parents.Len())
@@ -158,7 +209,7 @@ func getParentsSkipList(ctx context.Context, vrw types.ValueReadWriter, parents 
 		return nil
 	})
 	if err != nil {
-		return types.EmptyTuple(vrw.Format()), err
+		return types.Tuple{}, err
 	}
 	return types.NewTuple(vrw.Format(), entries...)
 }
